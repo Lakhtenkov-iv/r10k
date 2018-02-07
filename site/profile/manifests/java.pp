e09e891380454438363af6b2f175b41da894d1db
Lakhtenkov-iv <lakhtenkov@gmail.com>
2018-02-07 12:11:23 +0000

    add

---
+++ site/profile/manifests/java.pp
@@ -0,0 +1,22 @@
+class profile::java(
+  String $version = '8',
+  String $java_se = 'jdk'
+  Optional[String] $type = 'oracle'
+  Optional[String] $version_major = undef,
+  Optional[String] $version_minor = undef
+) {
+  if $type = 'openjdk' {
+    class { 'java':
+      distribution => $java_se,
+      #version => $version,
+    }
+  else { 
+    java::oracle {  "${java_se}${version}":
+      ensure        => 'present',
+      version       => $version,
+      version_major => $version_major,
+      version_minor => $version_minor,
+      java_se       => $java_se,
+    }
+  }
+}

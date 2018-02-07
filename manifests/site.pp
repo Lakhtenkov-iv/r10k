a5ebfe7c777c877ebe8cbc35c955ab0333c67819
Lakhtenkov-iv <lakhtenkov@gmail.com>
2018-02-07 23:21:09 +0000

    add

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,7 @@
 File { backup => false }
 node default {
 }
-#hiera_include('classes')
-node puppet.mshome.net {
-  include role::puppetmaster
-}
+hiera_include('classes')
+#node puppet.mshome.net {
+#  include role::puppetmaster
+#}

9cce1a158ee3cfc9ecd6548954ba37233164c0e1
Igor Lakhtenkov <lakhtenkov@gmail.com>
2018-02-09 16:08:56 +0300

    hey

--- modules/role/manifests/app_server.pp
+++ modules/role/manifests/app_server.pp
@@ -1,3 +1,15 @@
 class role::app_server {
+
+  stage { 'prerequisites' :
+    before => stage ['main'],
+  }
+
+  stage { 'configure_web_server' : }
+  stage { 'configure_web_server' : }
+
+  class { 'profile::base' :
+    stage => prerequisites
+  }
+
   include profile::base
 }

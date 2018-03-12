1e54fc09bae42e1bb87d7283150414aa624c8471
Lakhtenkov-iv <lakhtenkov@gmail.com>
2018-03-12 15:21:24 +0000

    111

--- site/profile/manifests/puppetdbserver.pp
+++ site/profile/manifests/puppetdbserver.pp
@@ -2,8 +2,10 @@ class profile::puppetdbserver (
     String $puppetdb_host = 'puppetdb.example.lan',
     String $postgres_host = 'postgres.example.lan'
 ) { 
+    class { 'puppetdb': 
+        manage_firewall => true,
+    }
     class { 'puppetdb::server':
         database_host => $postgres_host,
-        manage_firewall => true,
     } 
 }

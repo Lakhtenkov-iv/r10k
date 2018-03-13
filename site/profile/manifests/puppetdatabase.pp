32c74f624f0eac2ee517cc3011d8e784a000975f
Lakhtenkov-iv <lakhtenkov@gmail.com>
2018-03-13 10:41:21 +0000

    test

--- site/profile/manifests/puppetdatabase.pp
+++ site/profile/manifests/puppetdatabase.pp
@@ -1,8 +1,26 @@
 class profile::puppetdatabase (
     String $puppetdb_host = 'puppetdb.example.lan',
-    String $postgres_host = 'postgres.example.lan'
+    String $postgres_host = 'postgres.example.lan',
+    String $database_username = 'puppetdb',
+    String $database_password = 'puppetdb',
 ) { 
+   stage { 'first':
+     before => Stage['main'],
+   }
+   stage { 'last': }
+   Stage['main'] -> Stage['last']
+  
    class { 'puppetdb::database::postgresql':
+     stage => first,
      listen_addresses => $postgres_host,
-   } 
+     database_username => $database_username,
+     database_password => $database_password,
+   }
+   class { '::postgresql::server':
+     stage => last,
+     datadir => '/data',
+   }
+    
 }
+
+
--- site/profile/manifests/puppetdbserver.pp
+++ site/profile/manifests/puppetdbserver.pp
@@ -1,9 +1,13 @@
 class profile::puppetdbserver (
     String $puppetdb_host = 'puppetdb.example.lan',
-    String $postgres_host = 'postgres.example.lan'
+    String $postgres_host = 'postgres.example.lan',
+    String $database_username = 'puppetdb',
+    String $database_password = 'puppetdb',
 ) { 
     class { 'puppetdb::server':
         database_host => $postgres_host,
+        database_username => $database_username,
+        database_password => $database_password,
         open_listen_port => true,
         open_ssl_listen_port => true,
         manage_firewall => true,

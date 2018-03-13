52de907ae7c320d43b366df2077684e1d69ce9bc
Lakhtenkov-iv <lakhtenkov@gmail.com>
2018-03-13 14:01:19 +0000

    test

---
+++ site/profile/manifests/tomcat.pp
@@ -0,0 +1,11 @@
+class profile::tomcat (
+
+) {
+  tomcat::install { '/opt/tomcat8':
+    source_url => 'http://ftp.byfly.by/pub/apache.org/tomcat/tomcat-8/v8.5.29/bin/apache-tomcat-8.5.29.tar.gz'
+  tomcat::instance { 'current':
+    catalina_home => '/opt/tomcat8',
+    catalina_base => '/opt/tomcat8/current',
+  }
+  
+}

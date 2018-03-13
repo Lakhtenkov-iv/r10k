ef4d1d80c21e3dabcab387dd238d3fbf32265760
Lakhtenkov-iv <lakhtenkov@gmail.com>
2018-03-13 15:03:38 +0000

    test

--- site/profile/manifests/tomcat.pp
+++ site/profile/manifests/tomcat.pp
@@ -1,8 +1,10 @@
 class profile::tomcat (
-
+  String $tomcat_version = '8.5.29',
 ) {
+  $tomcat_major_version = split($tomcat_version, '.')[0] 
+  
   tomcat::install { '/opt/tomcat8':
-    source_url => 'http://ftp.byfly.by/pub/apache.org/tomcat/tomcat-8/v8.5.29/bin/apache-tomcat-8.5.29.tar.gz'
+    source_url => 'http://ftp.byfly.by/pub/apache.org/tomcat/tomcat-${tomcat_major_version}/v${tomcat_version}/bin/apache-tomcat-${tomcat_version}.tar.gz'
   }
   tomcat::instance { 'current':
     catalina_home => '/opt/tomcat8',

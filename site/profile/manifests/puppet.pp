b366ea2bb10d56e6ed889ecdf9f05131754d6eb3
Lakhtenkov-iv <lakhtenkov@gmail.com>
2018-03-13 08:32:15 +0000

    test

--- site/profile/manifests/puppet.pp
+++ site/profile/manifests/puppet.pp
@@ -1,16 +1,28 @@
 class profile::puppet (
     String $puppetdb_host = 'puppetdb.example.lan',
-    String $postgres_host = 'postgres.example.lan'
+    String $postgres_host = 'postgres.example.lan',
+    String $puppet_user = 'puppet',
+    String $puppet_user_group  = 'puppet'
 ) {
   class { 'hiera':
     hiera_version => '5',
     hiera5_defaults =>  {"datadir" => "/etc/puppetlabs/code/environments/%{environment}/hieradata", "data_hash" => "yaml_data"},
     hierarchy       =>  [
-                          {"name" =>  "Basic Hierarchy", "paths" =>  ['roles/%{facts.role}.yaml', 'nodes/%{::clientcert}.yaml', 'environments/%{::applicationtier}.yaml']},
+                          {"name" =>  "Basic Hierarchy", 
+                           "paths" =>  ['roles/%{facts.role}.yaml', 'nodes/%{::clientcert}.yaml', 'environments/%{::applicationtier}.yaml'], 
+                           "options" => {"pkcs7_private_key" => "$confdir/keys/private_key.pkcs7.pem", "pkcs7_public_key" => "$confdir/keys/public_key.pkcs7.pem" }},
                           {"name" =>  "Default yaml file", "path" =>  "common.yaml"},
                         ],
+    
     #eyaml => true,
     #keysdir => '/etc/puppetlabs/puppet/keys'
+  }
+
+  file { $puppet_conf_dir:
+    ensure => directory,
+    owner => $puppet_user,
+    group => $puppet_user_group
+    
   }
   class { 'puppetdb::master::config':
     puppetdb_server => $puppetdb_host,

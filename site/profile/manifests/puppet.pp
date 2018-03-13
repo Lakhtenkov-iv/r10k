53f5307d4bb64c9e7501e441464e9cb8d5ad86c1
Lakhtenkov-iv <lakhtenkov@gmail.com>
2018-03-13 07:01:46 +0000

    test

--- site/profile/manifests/puppet.pp
+++ site/profile/manifests/puppet.pp
@@ -9,7 +9,7 @@ class profile::puppet (
                           {"name" =>  "Basic Hierarchy", "paths" =>  ['roles/%{facts.role}.yaml', 'nodes/%{::clientcert}.yaml', 'environments/%{::applicationtier}.yaml']},
                           {"name" =>  "Default yaml file", "path" =>  "common.yaml"},
                         ],
-    eyaml                => true,
+    eyaml => true,
   }
   class { 'puppetdb::master::config':
       puppetdb_server => $puppetdb_host,

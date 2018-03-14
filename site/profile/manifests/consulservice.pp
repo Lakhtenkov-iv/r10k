929e9fb522a24d6d89031365888c691cf7cbb204
Lakhtenkov-iv <lakhtenkov@gmail.com>
2018-03-14 13:22:44 +0000

    fixing config

--- site/profile/manifests/consulservice.pp
+++ site/profile/manifests/consulservice.pp
@@ -1,8 +1,8 @@
 class profile::consulservice (
   String $service_name = 'service1',
   String $address = $facts.ipaddress,
-  String $port = '8080'
-  String $url = 'http://localhost:42/status'
+  String $port = '8080',
+  String $url = 'http://localhost:42/status',
 ) {
   consul::services:
   $service_name:

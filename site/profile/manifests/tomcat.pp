0da3fb1b568b0199ce359983ab97b1a7f0ea1fbb
Lakhtenkov-iv <lakhtenkov@gmail.com>
2018-03-14 10:50:33 +0000

    fixing config

--- site/profile/manifests/tomcat.pp
+++ site/profile/manifests/tomcat.pp
@@ -2,7 +2,10 @@ class profile::tomcat (
   String $tomcat_version = '8.5.29',
 ) {
   $tomcat_major_version = $tomcat_version.split('.')[0] 
-  notice ($tomcat_version.split('.'))  
+  notify {"Test" :
+      message =>  $tomcat_version.split('.'),
+    }
+  
   tomcat::install { '/opt/tomcat8':
     source_url => "http://ftp.byfly.by/pub/apache.org/tomcat/tomcat-${tomcat_major_version}/v${tomcat_version}/bin/apache-tomcat-${tomcat_version}.tar.gz"
   }

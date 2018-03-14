df52f4e336b767990ad9f4a319d64c20786d26da
Lakhtenkov-iv <lakhtenkov@gmail.com>
2018-03-14 13:31:53 +0000

    fixing config

--- site/profile/manifests/consulservice.pp
+++ site/profile/manifests/consulservice.pp
@@ -4,13 +4,15 @@ class profile::consulservice (
   String $port = '8080',
   String $url = 'http://localhost:42/status',
 ) {
-  consul::services:
-    $service_name:
-      address: "${address}"
-      checks:
-        - http: $url
-          interval: 5s
-      port: $port
-      tags:
-        - "test"
+  ::consul::service { $service_name:
+    address => $address
+    checks  => [
+    {
+      http     => $url,
+      interval => '10s'
+    }
+    ],
+    port    => $port,
+    tags    => ['test']
+  } 
 }

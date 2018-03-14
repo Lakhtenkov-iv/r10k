6234221860845ea0bba9d26a5b47d73a4a23bd5e
Lakhtenkov-iv <lakhtenkov@gmail.com>
2018-03-14 10:20:26 +0000

    fixing config

--- site/profile/manifests/tomcat.pp
+++ site/profile/manifests/tomcat.pp
@@ -4,7 +4,7 @@ class profile::tomcat (
   $tomcat_major_version = split($tomcat_version, '.')[0] 
   
   tomcat::install { '/opt/tomcat8':
-    source_url => 'http://ftp.byfly.by/pub/apache.org/tomcat/tomcat-${tomcat_major_version}/v${tomcat_version}/bin/apache-tomcat-${tomcat_version}.tar.gz'
+    source_url => "http://ftp.byfly.by/pub/apache.org/tomcat/tomcat-${tomcat_major_version}/v${tomcat_version}/bin/apache-tomcat-${tomcat_version}.tar.gz"
   }
   tomcat::instance { 'current':
     catalina_home => '/opt/tomcat8',

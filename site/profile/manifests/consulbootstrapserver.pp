b1e76f9fde1ccce13bb25b4c7474222b0e769e3b
Lakhtenkov-iv <lakhtenkov@gmail.com>
2018-03-14 07:43:59 +0000

    test

--- site/profile/manifests/consulbootstrapserver.pp
+++ site/profile/manifests/consulbootstrapserver.pp
@@ -1,15 +1,17 @@
 class profile::consulbootstrapserver (
   String $version = '1.0.6'
+  Srting $datacenter = 'ilakhtenkov'
 ) {
   class { '::consul':
+    version => $version
+  
     config_hash => {
-      'bootstrap_expect' => 1,
-      'data_dir'         => '/var/consul',
-      'datacenter'       => 'datacenter',
+      'bootstrap'        => true,
+      'data_dir'         => '/opt/consul',
+      'datacenter'       => '${datacenter}',
       'log_level'        => 'INFO',
-      'node_name'        => '$::fqdn',
+      'node_name'        => '${::fqdn}',
       'server'           => true,
-      'version'          => $version
     }
   }
 }

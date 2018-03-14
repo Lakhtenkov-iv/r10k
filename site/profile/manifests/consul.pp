41e302814c721b13fd5b5e2a1eee56830b2bd47b
Lakhtenkov-iv <lakhtenkov@gmail.com>
2018-03-14 11:21:15 +0000

    fixing config

--- site/profile/manifests/consul.pp
+++ site/profile/manifests/consul.pp
@@ -4,6 +4,8 @@ class profile::consul (
   String $datadir = '/opt/consul',
   String $consule_key = undef,
   String $node_name = $::fqdn,
+  String $ui_listen_host = '0.0.0.0'
+  Boolean $ui_install = false,
   Boolean $server = false,
   Boolean $bootstrap = false,
   Array $consul_servers = [
@@ -21,6 +23,8 @@ class profile::consul (
         'node_name'        => $node_name,
         'server'           => true,
         'encrypt'          => $consule_key,
+        'ui'               => $ui_install,
+        'client_addr'      => $ui_listen_host,
       }
     }
   } else {
